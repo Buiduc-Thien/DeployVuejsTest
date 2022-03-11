b9d2d1c1dd2ea0581e2dcf9e2deb6dc4e2d8d443
Thien <buithien160@gmail.com>
2022-03-11 12:47:55 +0700

    leader up code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
+    <h1>numerating objects: 15, done.
+Counting objects: 100% (15/15), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (14/14), done.
+Writing objects: 100% (15/15), 385.72 KiB | 5.14 MiB/s, done.
+Total 15 (delta 2), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (2/2), done.
+remote: 
+remote: Create a pull request for 'gh-pages' on GitHub by visiting:      
+remote:      https://github.com/Buiduc-Thien/DeployVuejsTest/pull/new/gh-pages</h1>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
